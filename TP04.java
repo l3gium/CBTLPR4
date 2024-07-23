ebe83a0a598aadcb48d08e3b431003f9f86561bc
Miguel Luizatto Alves <131935219+l3gium@users.noreply.github.com>
2024-07-23 19:08:45 -0300

    Update TP04.java

--- TP04.java
+++ TP04.java
@@ -8,15 +8,24 @@ public class TP04
         System.out.println("Mes: " + d2.retMes());
         System.out.println("Dia: " + d2.retDia() + "\n");
 
+        System.out.print("Bissexto: "+ d2.bissexto() + "\n");
         System.out.print(d2.mostra1() + "\n");
         System.out.print(d2.mostra2() + "\n");
-        System.out.print(d2.diasTranscorridos() + "\n");
-        d2.apresentaDataAtual();
+        System.out.print(d2.diasTranscorridos());
+        System.out.print("\n");
+        System.out.print("\n");
+       
+        Data d1 = new Data();
 
-         Data d1 = new Data();
+        System.out.println("\nAno: " + d1.retAno());
+        System.out.println("Mes: " + d1.retMes());
+        System.out.println("Dia: " + d1.retDia() + "\n");
 
-         System.out.println("Ano: " + d1.retAno());
-         System.out.println("Mes: " + d1.retMes());
-         System.out.println("Dia: " + d1.retDia());
+        System.out.print("Bissexto: " + d1.bissexto() + "\n");
+        System.out.print(d1.mostra1() + "\n");
+        System.out.print(d1.mostra2() + "\n");
+        System.out.print(d1.diasTranscorridos() + "\n");
+
+        d1.apresentaDataAtual();
     }
 }
